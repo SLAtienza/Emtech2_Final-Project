aba8de847d32c6d1248e54c7866d372408a5f621
qefernandez3 <158252105+qefernandez3@users.noreply.github.com>
2024-05-17 17:14:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('weights-improvement-46-0.91.hdf5')
-  return model
-model=load_model()
-st.write("""
-# Plant Leaf Detection System"""
-)
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+    model = tf.keras.models.load_model('weights-improvement-46-0.91.hdf5')
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(128,128)
-    image=ImageOps.fit(image_data,size,Image.LANCZOS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (128, 128)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    image = image.convert('RGB')
+    img = np.asarray(image)
+    img = img / 255.0 
+    img_reshape = np.reshape(img, (1, 64, 64, 3))
+    prediction = model.predict(img_reshape)
     return prediction
+
+model = load_model()
+
+st.write("""
+Detecting Pneumonia from Chest X-Ray Images
+""")
+
+file = st.file_uploader("Choose chest x-ray photo from computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['slug', 'snail']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['Slug', 'Snail']
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
     st.success(string)
