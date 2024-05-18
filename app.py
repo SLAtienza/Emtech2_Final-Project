a1f99de659397841a85cea41832162370bab5434
qefernandez3 <158252105+qefernandez3@users.noreply.github.com>
2024-05-18 12:25:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,26 @@ import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('weights-improvement-46-0.91.hdf5')
+    model = tf.keras.models.load_model('finalmodel.h5')
     return model
 
 def import_and_predict(image_data, model):
-    size = (128, 128)
+    size = (64, 64)
     image = ImageOps.fit(image_data, size, Image.LANCZOS)
     image = image.convert('RGB')
     img = np.asarray(image)
     img = img / 255.0 
-    img_reshape = np.reshape(img, (1, 128, 128, 3))
+    img_reshape = np.reshape(img, (1, 64, 64, 3))
     prediction = model.predict(img_reshape)
     return prediction
 
 model = load_model()
 
 st.write("""
-Detecting Pneumonia from Chest X-Ray Images
+Apple or Tomato
 """)
 
-file = st.file_uploader("Choose chest x-ray photo from computer", type=["jpg", "png"])
+file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
 
 if file is None:
     st.text("Please upload an image file")
